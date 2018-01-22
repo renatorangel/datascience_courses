d5584c6d51863761b89eee948efe0c419be0af0b
Renato Rangel <renatorangelbomfim@gmail.com>
2018-01-21 23:35:01 -0200

    starting gradient descent python script

---
+++ machine_learning_andrew/ex1/gradient_descent.py
@@ -0,0 +1,18 @@
+import numpy as np
+from ggplot import mtcars
+
+
+def hypothesis(theta):
+    pass
+
+
+def cost_function(X, Y, m, theta):
+    pass
+
+def gradient_descent():
+    pass
+
+
+if __name__ == "__main__":
+    mtcars.head()
+    #cost_function()
