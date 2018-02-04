9409e5da67e975f342bc21d2e7574f25a901f0fe
Renato Rangel <renatorangelbomfim@gmail.com>
2018-02-04 12:19:50 -0200

    added gradient descent visualization

--- machine_learning_andrew/ex1/gradient_descent.py
+++ machine_learning_andrew/ex1/gradient_descent.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ggplot import mtcars
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def hypothesis(theta, X):
@@ -14,11 +15,26 @@ def cost_function_j_(X, Y, m, theta):
     return np.sum(np.power(hypothesis(theta, X) - Y, 2)) / (2 * m)
 
 
+def my_formula(x, theta):
+        return theta[0, 1] * x + theta[0, 0]
+
+
+def graph(formula, x_range, theta):
+    y = formula(X[:, 1], theta)
+
+    plt.plot(X[:, 1], Y, 'ro', x_range, y)
+    plt.ylabel('hp')
+    plt.xlabel('mpg')
+    plt.show()
+
+
 def gradient_descent(X, Y, m, theta, alpha, iterations):
+    cost_list = []
 
     for i in range(0, iterations):
         cost = cost_function_j_(X, Y, m, theta)
         print(cost)
+        cost_list.append(cost)
 
         b = hypothesis(theta, X) - Y
 
@@ -27,6 +43,12 @@ def gradient_descent(X, Y, m, theta, alpha, iterations):
 
         theta[0, 0] = theta_0
         theta[0, 1] = theta_1
+
+    plt.plot(range(0, iterations), cost_list)
+    plt.ylabel('cost')
+    plt.show()
+
+    graph(my_formula, X[:, 1], theta)
 
     return theta
 
@@ -47,7 +69,7 @@ if __name__ == "__main__":
     X = np.column_stack((np.ones(m), mtcars["mpg"]))
     Y = mtcars["hp"].as_matrix().reshape((32, 1))
     alpha = 0.001
-    iterations = 1500
+    iterations = 100000
     theta = np.matrix([1, 1]).astype("float64")
     new_theta = gradient_descent(X, Y, m, theta, alpha, iterations)
     print(new_theta)
